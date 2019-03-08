3f581f4aa121090f69f3086ce82e1c1e7221baa3
ZillyMee <zillameetun@gmail.com>
2019-03-08 15:21:48 +0000

    Update carControlCtrl.js

--- src/app/shared/carControl/carControlCtrl.js
+++ src/app/shared/carControl/carControlCtrl.js
@@ -156,6 +156,17 @@ function CarControlViewCtrl($scope, $state, $stateParams, mqttService, brokerDet
 
     });
 
+    Toast({
+        text: null, 
+        duration: 3000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top", // top or bottom
+        positionLeft: true, // true or false
+        backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
+      }).showToast();
+
     /*
      When users changes car throttle a change request is sent to server. 
     */
