472f28c15c3d1392c1b40aba46b29f6fd7d3a06d
SkrrttRacing <44700865+SkrrttRacing@users.noreply.github.com>
2019-03-13 13:38:32 +0000

    Update brokerDetailsConstant.js

--- src/app/components/brokerDetails/brokerDetailsConstant.js
+++ src/app/components/brokerDetails/brokerDetailsConstant.js
@@ -1,1 +1,1 @@
-angular.module('app').value('brokerDetails',{"UUID":"testUUID","HOST":"test.mosquitto.org","PORT":"8080","USERNAME":"ZillyMee","PASSWORD":"","SSL":true});
+angular.module('app').value('brokerDetails',{"UUID":"","HOST":"","PORT":"","USERNAME":"","PASSWORD":"","SSL":true});
