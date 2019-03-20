0aa0794cc82d84594f9a35ba35731e247e99d127
SkrrttRacing <44700865+SkrrttRacing@users.noreply.github.com>
2019-03-20 11:57:04 +0000

    Update carControlCtrl.js

--- src/app/shared/carControl/carControlCtrl.js
+++ src/app/shared/carControl/carControlCtrl.js
@@ -154,13 +154,7 @@ function CarControlViewCtrl($scope, $state, $stateParams, mqttService, brokerDet
 
     Toast(function (message)){
         text: message, 
-        duration: 3000,
-        destination: "https://github.com/apvarun/toastify-js",
-        newWindow: true,
-        close: true,
-        gravity: "top", // top or bottom
-        positionLeft: true, // true or false
-        backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
+        
       .showToast();}
 
     /*
